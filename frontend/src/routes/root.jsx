1ca40c7f0acba110a399efdae929b8d92b23ccea
Pavel Hujer <101639003+Grefee@users.noreply.github.com>
2023-03-16 11:33:45 +0100

    Update root.jsx

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -30,30 +30,30 @@ export default function Home() {
 
 
 function MainHome() {
-  const {isLoading: reportsLoad, data: typyLinek} = useQuery({queryKey: ['typyLinek'], queryFn: async () => await getTypyLinek(IP), initialData: []});
+    const {isLoading: reportsLoad, data: typyLinek} = useQuery({queryKey: ['typyLinek'], queryFn: async () => await getTypyLinek(IP), initialData: []});
 
     const [selectedLinka, setSelectedLinka] = useState(null);
 
+  
     const handleButtonClick = id => {
         setSelectedLinka(id);
       };
 
+  
     const goToHome = () => {
     setSelectedLinka(null);
     }
 
-    return (
-        
+    
+    return (    
         <div className="flex flex-col items-center w-full">
             {!selectedLinka ? (
                 <div>
                     <div className="bg-gradient-to-r from-gray-700 to-gray-500 pt-16 pb-16 pl-8 pr-8 text-3xl w-screen text-center text-white ">Vyberte Linku</div>
                         <div className="flex flex-col items-center">
                         {typyLinek.map((linka) => {
-                            return <button className="pt-5 pb-5 pl-5 pr-5 bg-green-200 w-fit rounded-full mt-5" id={linka.PLine_id}  onClick={() => handleButtonClick(linka.PLine_id)}> {linka.PLine_name}</button>
-                            
-                        })}
-                        
+                            return <button className="pt-5 pb-5 pl-5 pr-5 bg-green-200 w-fit rounded-full mt-5" id={linka.PLine_id}  onClick={() => handleButtonClick(linka.PLine_id)}> {linka.PLine_name}</button>                            
+                        })}   
                     </div>
                 </div>
              ) : (
